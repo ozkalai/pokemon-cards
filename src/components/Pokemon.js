102c320d1e1831aa81f550554b5c6967ca41df9a
ozkalai <melihozkalayy@gmail.com>
2021-04-14 21:56:39 +0300

    Pokemon component updated

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -1,9 +1,33 @@
 import React from 'react';
 
-const Pokemon = () => {
+const Pokemon = ({pokemon}) => {
  return(
-    <p>HEloo</p>
- )
+    <div className="pokemon">
+        <div className="pokemon--name">
+            <p>{pokemon.name}</p>
+        </div>
+        <div className="pokemon--meta">
+            <span>{pokemon.maxHP}</span>
+            <span>{pokemon.maxCP}</span>
+        </div>
+        <div className="pokemon--image">
+            <img src={pokemon.image} alt={pokemon.name}/>
+        </div>
+        <div className="pokemon--attacks">
+            {pokemon && 
+            pokemon.attacks && 
+            pokemon.attacks.special
+            .slice(0,3)
+            .map(attack => (
+                <span key={`${attack.name}-${attack.damage}`} >
+                    {attack.name }
+                </span>
+            ))
+            }
+        </div>
+
+    </div>
+    )
 }
 
 export default Pokemon;
